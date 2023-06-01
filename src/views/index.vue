ddca7df1467e2aaf06aed19a476e22e8231c4237
Your Name <you@example.com>
2023-06-01 22:26:12 +0800

    del jiemian

--- src/views/index.vue
+++ src/views/index.vue
@@ -5,7 +5,7 @@ import { Cloud } from "laf-client-sdk";
 import { marked } from "marked";
 
 const cloud = new Cloud({
-  baseUrl: "https://jyf6wk.laf.dev",
+  baseUrl: "https://l5hrw3.laf.dev",
   getAccessToken: () => "",
   timeout: 60000,
 });
@@ -58,6 +58,8 @@ async function send() {
     return;
   }
 
+  // console.log(res)
+
   parentMessageId.value = res.id;
 
   // res.text = res.text.replace(/\n/g, "<br>");
@@ -82,7 +84,7 @@ function setScreen() {
 <template>
   <div class="page">
     <div class="begintitle">
-      <h1 v-show="!list.length" @click="send">左风的ChatGPT</h1>
+      <h1 v-show="!list.length" @click="send">时维的ChatGPT</h1>
     </div>
 
     <div id="myList">
@@ -97,83 +99,6 @@ function setScreen() {
       </div>
     </div>
 
-    <div v-show="!list.length" class="exhibition">
-      <div class="witem">
-        <svg
-          class="w-6 h-6 m-auto"
-          fill="none"
-          height="1em"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="1.5"
-          viewBox="0 0 24 24"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="12" cy="12" r="5"></circle>
-          <line x1="12" x2="12" y1="1" y2="3"></line>
-          <line x1="12" x2="12" y1="21" y2="23"></line>
-          <line x1="4.22" x2="5.64" y1="4.22" y2="5.64"></line>
-          <line x1="18.36" x2="19.78" y1="18.36" y2="19.78"></line>
-          <line x1="1" x2="3" y1="12" y2="12"></line>
-          <line x1="21" x2="23" y1="12" y2="12"></line>
-          <line x1="4.22" x2="5.64" y1="19.78" y2="18.36"></line>
-          <line x1="18.36" x2="19.78" y1="5.64" y2="4.22"></line>
-        </svg>
-        <h3 class="title">实例</h3>
-        <p>"用简单的术语解释量子计算"</p>
-        <p>"10岁的生日有什么创意吗？"</p>
-        <p>"如何在Javascript中提出HTTP请求？"</p>
-      </div>
-
-      <div class="witem">
-        <svg
-          aria-hidden="true"
-          class="w-6 h-6 m-auto"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="1.5"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          ></path>
-        </svg>
-        <h3 class="title">功能</h3>
-        <p>还记得用户在对话中早些时候说的话</p>
-        <p>允许用户提供后续更正</p>
-        <p>接受过拒绝不当请求的培训</p>
-      </div>
-
-      <div class="witem">
-        <svg
-          class="w-6 h-6 m-auto"
-          fill="none"
-          height="1em"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="1.5"
-          viewBox="0 0 24 24"
-          width="1em"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"
-          ></path>
-          <line x1="12" x2="12" y1="9" y2="13"></line>
-          <line x1="12" x2="12.01" y1="17" y2="17"></line>
-        </svg>
-        <h3 class="title">限制</h3>
-        <p>偶尔可能会生成错误的信息</p>
-        <p>偶尔可能会产生有害的指令或有偏见的内容</p>
-        <p>对2021年后的世界和事件的了解有限</p>
-      </div>
-    </div>
     <div class="steppingstone"></div>
 
     <div class="inputbox">
